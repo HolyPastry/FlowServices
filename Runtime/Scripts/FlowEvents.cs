2d069c84ecfc55b1c6bbb1176c65548703ec3518
Sebastien Berton <sebasti1.berton@gmail.com>
2025-03-20 12:59:18 +0000

    added default values

--- Runtime/Scripts/FlowEvents.cs
+++ Runtime/Scripts/FlowEvents.cs
@@ -9,42 +9,42 @@ public static partial class FlowEvents
     /// <summary>
     /// Event triggered when a scene transition starts to transition out.
     /// </summary>
-    public static Action<SceneTransition> OnTransitionOut;
+    public static Action<SceneTransition> OnTransitionOut = delegate { };
 
     /// <summary>
     /// Event triggered when a scene transition starts to transition in.
     /// </summary>
-    public static Action<SceneTransition> OnTransitionIn;
+    public static Action<SceneTransition> OnTransitionIn = delegate { };
 
     /// <summary>
     /// Event triggered when a scene transition starts to fade out.
     /// </summary>
-    public static Action<SceneTransition> OnFadeOut;
+    public static Action<SceneTransition> OnFadeOut = delegate { };
 
     /// <summary>
     /// Event triggered when a scene transition starts to fade in.
     /// </summary>
-    public static Action<SceneTransition> OnFadeIn;
+    public static Action<SceneTransition> OnFadeIn = delegate { };
 
     /// <summary>
     /// Event triggered when the loading process is finished.
     /// </summary>
-    public static Action OnFinishLoading;
+    public static Action OnFinishLoading = delegate { };
 
     /// <summary>
     /// Event triggered when the game is over.
     /// </summary>
-    public static Action OnGameOver;
+    public static Action OnGameOver = delegate { };
 
     /// <summary>
     /// Event triggered when entering a transition zone.
     /// </summary>
-    public static Action<string> OnEnteredTransitionZone;
+    public static Action<string> OnEnteredTransitionZone = delegate { };
 
     /// <summary>
     /// Event triggered when exiting a transition zone.
     /// </summary>
-    public static Action OnExitedTransitionZone;
+    public static Action OnExitedTransitionZone = delegate { };
 }
 
 
--- Runtime/Scripts/FlowServices.cs
+++ Runtime/Scripts/FlowServices.cs
@@ -10,59 +10,59 @@ public static partial class FlowServices
     /// <summary>
     /// Action to load a scene with the provided <see cref="SceneData"/>.
     /// </summary>
-    public static Action<SceneData> LoadScene;
+    public static Action<SceneData> LoadScene = (sceneData) => { };
 
     /// <summary>
     /// Function to check if the service is enabled.
     /// </summary>
     /// <returns>True if the service is enabled, otherwise false.</returns>
-    public static Func<bool> IsEnabled;
+    public static Func<bool> IsEnabled = () => false;
 
     /// <summary>
     /// Function to wait until the service is ready.
     /// </summary>
     /// <returns>A <see cref="WaitUntil"/> object that waits until the service is ready.</returns>
-    public static Func<WaitUntil> WaitUntilReady;
+    public static Func<WaitUntil> WaitUntilReady = () => new WaitUntil(() => true);
 
     /// <summary>
     /// Internal action to load the next scene.
     /// </summary>
-    public static Action LoadNextScene;
+    public static Action LoadNextScene = () => { };
 
     /// <summary>
     /// Internal function to get the name of the next scene.
     /// </summary>
     /// <returns>The name of the next scene.</returns>
-    public static Func<string> NextSceneName;
+    public static Func<string> NextSceneName = () => string.Empty;
 
     /// <summary>
     /// Internal function to get the current scene data.
     /// </summary>
     /// <returns>The current <see cref="SceneData"/>.</returns>
-    public static Func<SceneData> GetCurrentScene;
+    public static Func<SceneData> GetCurrentScene = () => null;
 
     /// <summary>
     /// Internal function to get the scene data from a given scene name.
     /// </summary>
     /// <param name="name">The name of the scene.</param>
     /// <returns>The <see cref="SceneData"/> for the given scene name.</returns>
-    public static Func<string, SceneData> GetSceneFromName;
+    public static Func<string, SceneData> GetSceneFromName = (name) => null;
 
     /// <summary>
     /// Action to handle visual transition out with the provided <see cref="SceneTransition"/>.
     /// </summary>
-    public static Action<SceneTransition> FadeOut;
+    public static Action<SceneTransition> FadeOut = (transition) => { };
 
     /// <summary>
     /// Action to handle visual transition in with the provided <see cref="SceneTransition"/>.
     /// </summary>
-    public static Action<SceneTransition> FadeIn;
+    public static Action<SceneTransition> FadeIn = (transition) => { };
 
     /// <summary>
     /// Function to check if a custom transition out is enabled.
     /// </summary>
     /// <returns>True if a custom transition out is enabled, otherwise false.</returns>
-    public static Func<bool> IsCustomTransitionOut;
+    public static Func<bool> IsCustomTransitionOut = () => false;
 }
 
 
